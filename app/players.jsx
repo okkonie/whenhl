7f0278b889910dfadb6f72b7bc027368da399c66
okkonie <okko.niemikorpi1@gmail.com>
2025-10-12 18:19:52 +0300

    dropdowns

--- app/players.jsx
+++ app/players.jsx
@@ -1,8 +1,9 @@
 import { View, Text, StyleSheet, TouchableOpacity, TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useState, useEffect } from "react";
-import PlayerOptions from "../components/playerOptions";
 import Ionicons from '@expo/vector-icons/Ionicons';
+import SeasonDropdown from "../components/seasonDropDown";
+import StatsDropdown from "../components/statsDropDown";
 
 export default function Players() {
   const [loading, setLoading] = useState(false);
@@ -11,16 +12,21 @@ export default function Players() {
   const [query, setQuery] = useState('')
   const [results, setResults] = useState([])
   const [skaterOn, setSkaterOn] = useState(true)
-  const [season, setSeason] = useState('current')
+  const [season, setSeason] = useState({"key": "current", "label": "current"})
   const [stats, setStats] = useState([])
-  const [optionsVisible, setOptionsVisible] = useState(false)
+  const [modalVisible, setModalVisible] = useState(false)
+  const [statsModalVisible, setStatsModalVisible] = useState(false)
+  const [skaterSort, setSkaterSort] = useState('points')
+  const [goalieSort, setGoalieSort] = useState('save%')
 
   useEffect(() => {
     const fetchStats = async () => {
+      if (!season?.key) return;
+      
       try {
         setLoading(true)
         const statType = skaterOn ? 'skater-stats-leaders' : 'goalie-stats-leaders';
-        const response = await fetch(`https://api-web.nhle.com/v1/${statType}/${season}?limit=30`);
+        const response = await fetch(`https://api-web.nhle.com/v1/${statType}/${season.key}?limit=30`);
         const data = await response.json();
         setStats(data);
 
@@ -40,13 +46,6 @@ export default function Players() {
           <>
             <Text style={s.headerText}>Top players</Text>
             <View style={s.buttons}> 
-              <TouchableOpacity
-                onPress={() => setOptionsVisible(true)}
-                style={s.button}
-                activeOpacity={0.5}
-              >
-                <Ionicons name="options" size={24} color="white" />
-              </TouchableOpacity>
               <TouchableOpacity
                 onPress={() => { setTempText(''); setSearching(true); }}
                 style={s.button}
@@ -83,7 +82,25 @@ export default function Players() {
       
       {!searching ? (
         <>
-          <PlayerOptions visible={optionsVisible} onClose={() => setOptionsVisible(false)}/>
+          <View style={s.dropdownContainer}>
+            <TouchableOpacity style={[s.toggle, {justifyContent: skaterOn ? 'flex-start' : 'flex-end'}]} onPress={() => {setSkaterOn(!skaterOn)}} activeOpacity={0.7}>
+              <Text style={s.toggleText}>{skaterOn ? 'skater' : 'goalie'}</Text>
+            </TouchableOpacity>
+
+            <SeasonDropdown 
+              season={season}
+              setSeason={setSeason}
+              modalVisible={modalVisible}
+              setModalVisible={setModalVisible}
+            />
+            <StatsDropdown
+              skaterOn={skaterOn}
+              selected={skaterOn ? skaterSort : goalieSort}
+              onSelect={(val) => skaterOn ? setSkaterSort(val) : setGoalieSort(val)}
+              modalVisible={statsModalVisible}
+              setModalVisible={setStatsModalVisible}
+            />
+          </View>
         </>
 
       ) : (
@@ -94,10 +111,45 @@ export default function Players() {
 }
 
 const s = StyleSheet.create({
+  dropdownContainer: {
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    gap: 12,
+    borderBottomWidth: 1,
+    borderColor: '#222'
+  },
+  toggle: {
+    flexDirection: 'column',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#222',
+    overflow: 'hidden',
+    width: 70,
+    padding: 3
+  },
+  toggleText: {
+    color: 'white',
+    fontSize: 11,
+    fontWeight: '700',
+    textAlign: 'center',
+    backgroundColor: '#1a1a1a',
+    padding: 5,
+    borderRadius: 5,
+  },
+  label: {
+    color: '#b0b0b0',
+    fontSize: 13,
+    fontWeight: '600',
+    marginBottom: 8,
+    textTransform: 'uppercase',
+  },
   buttons: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 24
+    borderBottomWidth: 1,
+    borderColor: '#222'
   },
   container: {
     backgroundColor: "#111",
@@ -122,8 +174,7 @@ const s = StyleSheet.create({
     height: 30,
     alignItems: 'center',
     justifyContent: 'center'
-  }
-  ,
+  },
   searchInput: {
     flex: 1,
     color: 'white',
